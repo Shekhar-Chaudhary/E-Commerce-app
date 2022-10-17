8325faf98b5d081737c6d05e89b1aab083ac210d
Shekhar-Chaudhary <shekhar.chaudhary@yash.com>
2022-10-17 18:37:57 +0530

    BackendUser Authentication

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import { Link, useLocation } from 'react-router-dom'
+import FormContainer from "../Components/FormContainer/FormContainer";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const location = useLocation();
+
+  const redirect = location.search ? location.search.split('=')[1] : '/'
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    //DISPATCH LOGIN
+  }
+
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type="submit" variant="primary">
+          Sign In
+        </Button>
+      </Form>
+
+      <Row className="py-3 ">
+        <Col> 
+            New Customer ? <Link to={redirect ? `/register/?redirect=${redirect}` : '/register'}>Register</Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default LoginScreen;
